fd861ec80f20ea750584e1e0ecc7a330668d61db
Shrimpy <45856863+Shrimpy48@users.noreply.github.com>
2019-04-20 00:04:07 +0100

    Evaluation in progress

--- main.py
+++ main.py
@@ -5,48 +5,23 @@ from pycuda.compiler import SourceModule
 
 
 class Function:
-    def __init__(self, args, body):
-        self.objects = {}
-        for arg in args:
-            val = self.value(arg)
-            if val is None:
-                self.objects[arg] = val
+    def __init__(self, args, body=None):
         self.args = args
         self.body = body
-
-    @classmethod
-    def value(cls, string):
-        stripped = string.strip()
-        if stripped[0] == stripped[-1] == '"':
-            return stripped[1:-1]
-        if stripped[0] == "[" and stripped[-1] == "]":
-            parts = stripped[1:-1].split(",")
-            val = []
-            for part in parts:
-                val.append(cls.value(part))
-            return val
-        try:
-            val = int(stripped)
-        except ValueError:
-            pass
-        else:
-            return val
-        try:
-            val = float(stripped)
-        except ValueError:
-            pass
-        else:
-            return val
-        return None
 
 
 class Program:
     def __init__(self, code_lines):
         self.objects = {}
+        self.actions = []
         for line in code_lines:
-            self.parse(line)
+            stripped = line.strip()
+            if stripped != "":
+                self.parse_dec(stripped)
 
-    def parse(self, line):
+        print(self.objects)
+
+    def parse_dec(self, line):
         parts = line.split("=")
         if len(parts) > 2:
             raise SyntaxError
@@ -56,9 +31,98 @@ class Program:
             dec_parts = separate(declaration)
             name = dec_parts[0]
             args = dec_parts[1:]
-            self.objects[name] = Function(args, definition)
+            arg_vals = []
+            arg_vars = []
+            for arg in args:
+                val = self.literal(arg)
+                if val is None:
+                    arg_vars.append(arg)
+                arg_vals.append(val)
+            if name not in self.objects:
+                self.objects[name] = []
+            self.objects[name].append((arg_vals, Function(arg_vars, body=definition)))
         else:
-            raise NotImplementedError
+            action_parts = parts[0].split("<-")
+            if len(action_parts) != 2:
+                raise SyntaxError
+            action, data = action_parts
+            self.actions.append((action.strip(), data.strip()))
+
+    def run(self):
+        for action, data in self.actions:
+            if action == "output":
+                value = self.evaluate(data, {})
+                print(value)
+            else:
+                raise RuntimeError("Unknown action", action)
+
+    def evaluate(self, exp, local_vars):
+        print("Evaluating", exp)
+        while exp[0] == "(" and exp[-1] == ")":
+            exp = exp[1:-1]
+        parts = separate(exp)
+        val = self.literal(parts[0])
+        if val is not None:
+            return val
+        args = parts[1:]
+        arg_vals = []
+        for arg in args:
+            val = self.evaluate(arg, local_vars)
+            arg_vals.append(val)
+        func_name = parts[0]
+        if func_name in local_vars:
+            funcs = local_vars[func_name]
+        elif func_name in self.objects:
+            funcs = self.objects[func_name]
+        else:
+            raise RuntimeError("Could not find function", func_name)
+        func = None
+        func_args = None
+        for func_opt_args, func_opt in funcs:
+            match = True
+            if len(arg_vals) != len(func_opt_args):
+                match = False
+            for i in range(len(func_opt_args)):
+                if func_opt_args[i] is not None and func_opt_args[i] != arg_vals[i]:
+                    match = False
+            if match:
+                func = func_opt
+                func_args = func_opt_args
+                break
+        if func is None:
+            raise RuntimeError("Could not match arguments")
+        variables = {}
+        count = 0
+        for i in range(len(func_args)):
+            if func_args[i] is None:
+                variables[func.args[count]] = arg_vals[i]
+                count += 1
+        return self.evaluate(func.body, variables)
+
+    @classmethod
+    def literal(cls, string):
+        stripped = string.strip()
+        if stripped[0] == stripped[-1] == '"':
+            return stripped[1:-1]
+        if stripped[0] == "[" and stripped[-1] == "]":
+            parts = separate(stripped[1:-1])
+            val = []
+            for part in parts:
+                val.append(cls.literal(part))
+            return numpy.array(val)
+        try:
+            val = numpy.int(stripped)
+        except ValueError:
+            pass
+        else:
+            return val
+        try:
+            val = numpy.float32(stripped)
+        except ValueError:
+            pass
+        else:
+            return val
+        return None
 
 
 def separate(string):
@@ -93,3 +157,4 @@ with open(filename, "r") as f:
     code = f.readlines()
 
 program = Program(code)
+program.run()
