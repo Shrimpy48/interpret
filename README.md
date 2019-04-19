73ae00b3ead8c446c7627dbc603268d83bb8ae6a
Shrimpy <45856863+Shrimpy48@users.noreply.github.com>
2019-04-19 21:50:06 +0100

    Initial parsing progress

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # interpret
-A Python 3 + CUDA interpreter for a simple language with GPU acceleration.
+A Python 3 + CUDA interpreter with GPU acceleration. Interprets a stateless, loosely Haskell-like language.
--- main.py
+++ main.py
@@ -4,9 +4,92 @@ import numpy
 from pycuda.compiler import SourceModule
 
 
-filename = input("File to execute: ")
+class Function:
+    def __init__(self, args, body):
+        self.objects = {}
+        for arg in args:
+            val = self.value(arg)
+            if val is None:
+                self.objects[arg] = val
+        self.args = args
+        self.body = body
+
+    @classmethod
+    def value(cls, string):
+        stripped = string.strip()
+        if stripped[0] == stripped[-1] == '"':
+            return stripped[1:-1]
+        if stripped[0] == "[" and stripped[-1] == "]":
+            parts = stripped[1:-1].split(",")
+            val = []
+            for part in parts:
+                val.append(cls.value(part))
+            return val
+        try:
+            val = int(stripped)
+        except ValueError:
+            pass
+        else:
+            return val
+        try:
+            val = float(stripped)
+        except ValueError:
+            pass
+        else:
+            return val
+        return None
+
+
+class Program:
+    def __init__(self, code_lines):
+        self.objects = {}
+        for line in code_lines:
+            self.parse(line)
+
+    def parse(self, line):
+        parts = line.split("=")
+        if len(parts) > 2:
+            raise SyntaxError
+        elif len(parts) == 2:
+            declaration = parts[0].strip()
+            definition = parts[1].strip()
+            dec_parts = separate(declaration)
+            name = dec_parts[0]
+            args = dec_parts[1:]
+            self.objects[name] = Function(args, definition)
+        else:
+            raise NotImplementedError
+
+
+def separate(string):
+    parts = []
+    paren_depth = 0
+    in_str = False
+    in_space = True
+    part_start = 0
+    for i in range(len(string)):
+        char = string[i]
+        if not in_space and char == " ":
+            in_space = True
+            if paren_depth == 0 and not in_str:
+                parts.append(string[part_start:i])
+        elif in_space and char != " ":
+            in_space = False
+            if paren_depth == 0 and not in_str:
+                part_start = i
+        if char in "[(":
+            paren_depth += 1
+        elif char in "])":
+            paren_depth -= 1
+        elif char == '"':
+            in_str = not in_str
+    if not in_space:
+        parts.append(string[part_start:])
+    return parts
+
+
+filename = "testprog.shr"  # input("File to execute: ")
 with open(filename, "r") as f:
     code = f.readlines()
 
-for line in code:
-    print(line, end="")
+program = Program(code)
